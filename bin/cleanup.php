c8bc81610e742469b56c66daa6b7a2f9d88ec035
NALLAM ABHILASH <abhilashnallam1@gmail.com>
2026-08-19 17:49:35 +0530

    Fix cleanup interval SQL

--- bin/cleanup.php
+++ bin/cleanup.php
@@ -6,16 +6,13 @@ require dirname(__DIR__) . '/bootstrap.php';
 use OtpAuth\Database;
 
 $db=Database::connection();
-$otpDays=(int)($_ENV['OTP_RETENTION_DAYS'] ?? 7);
-$rateDays=(int)($_ENV['RATE_LIMIT_RETENTION_DAYS'] ?? 2);
-$sessionDays=(int)($_ENV['SESSION_RETENTION_DAYS'] ?? 2);
+$otpDays=max(1,min(365,(int)($_ENV['OTP_RETENTION_DAYS'] ?? 7)));
+$rateDays=max(1,min(30,(int)($_ENV['RATE_LIMIT_RETENTION_DAYS'] ?? 2)));
+$sessionDays=max(1,min(30,(int)($_ENV['SESSION_RETENTION_DAYS'] ?? 2)));
 
-$deleted=[];
-$deleted['otp_challenges']=$db->prepare('DELETE FROM otp_challenges WHERE expires_at < UTC_TIMESTAMP() - INTERVAL ? DAY');
-$deleted['otp_challenges']->execute([$otpDays]);
-$deleted['rate_limits']=$db->prepare('DELETE FROM rate_limits WHERE window_started_at < UTC_TIMESTAMP() - INTERVAL ? DAY');
-$deleted['rate_limits']->execute([$rateDays]);
-$deleted['sessions']=$db->prepare('DELETE FROM customer_sessions WHERE expires_at < UTC_TIMESTAMP() - INTERVAL ? DAY');
-$deleted['sessions']->execute([$sessionDays]);
-
-echo "Cleanup completed.\n";
+$queries=[
+ 'otp_challenges'=>"DELETE FROM otp_challenges WHERE expires_at < UTC_TIMESTAMP() - INTERVAL {$otpDays} DAY",
+ 'rate_limits'=>"DELETE FROM rate_limits WHERE window_started_at < UTC_TIMESTAMP() - INTERVAL {$rateDays} DAY",
+ 'sessions'=>"DELETE FROM customer_sessions WHERE expires_at < UTC_TIMESTAMP() - INTERVAL {$sessionDays} DAY",
+];
+foreach($queries as $name=>$sql){$db->exec($sql); echo $name.': '.$db->rowCount()." deleted\n";}
